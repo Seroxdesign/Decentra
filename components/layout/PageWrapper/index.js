ad6258475fce05f00b7a3589408376b97a3072cc
D <95552251+d3centra@users.noreply.github.com>
2022-05-13 17:26:36 +0200

    Update index.js

--- components/layout/PageWrapper/index.js
+++ components/layout/PageWrapper/index.js
@@ -6,6 +6,11 @@ import styles from './styles.module.css'
 export default function PageWrapper(props) {
   return (
     <div className={styles.container}>
+  
+  <div className={styles.horizontal_navbar}>
+    <div className={styles.active_communitylogo}></div>
+    <a className={styles.active_communityname}>Decentra</a>
+  </div>
 
       <div className={styles.inner_container}>
         <div className={styles.main_container}>
