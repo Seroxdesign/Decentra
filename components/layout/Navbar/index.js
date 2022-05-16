1119c2136b1fb04997077f3b4f4e69719a2f91f6
D <95552251+d3centra@users.noreply.github.com>
2022-05-16 16:05:41 +0200

    Update index.js

--- components/layout/Navbar/index.js
+++ components/layout/Navbar/index.js
@@ -134,9 +134,6 @@ export default function Navbar() {
           <button className={styles.mobile_control} onClick={() => {toggleMenu(true)}}>
             <img src="https://i.imgur.com/rhTxXmI.png" alt="open mobile menu"/>
           </button>
-          <a href="/404">
-            🏡
-          </a>
         }
         
 
