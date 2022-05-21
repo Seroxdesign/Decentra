375bcd99628b3b23cda8f85de42a06e4a5faba38
D <95552251+d3centra@users.noreply.github.com>
2022-05-21 14:25:05 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -47,6 +47,7 @@ export default function index({posts, links}) {
     <h3>About</h3>
     <p>Forum for talking to the developers. All messages in this channel must be addressed to the devs. Or not.</p>
   </div>
+  </div>
  </div>
   )
 }
